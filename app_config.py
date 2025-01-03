bc6945f1e5ca2ce0839f1bfa356926a8482e6452
D Yashaswini <157148346+DYashaswini@users.noreply.github.com>
2025-01-03 12:47:33 +0530

    Created app_config.py

---
+++ app_config.py
@@ -0,0 +1,5 @@
+required_skills_dict = {
+    "Data Scientist": ["python", "data analysis", "machine learning", "statistics", "sql"],
+    "Software Engineer": ["python", "java", "c++", "problem-solving", "algorithms"],
+    "Web Developer": ["html", "css", "javascript", "react", "node.js"]
+}
